ff627c66e0eeb9e207def47098f974fa2f037f64
hoangviet-nguyen <viety.nguyen02@gmail.com>
2023-11-17 11:07:46 +0100

    refactoring and testing the classes in jupyter

--- DataLoader.py
+++ DataLoader.py
@@ -2,10 +2,13 @@ from random import randint
 import numpy as np
 import torchvision.datasets as datasets
 import torchvision.transforms as transform
+from torch.utils.data import DataLoader
+import torch.nn as nn
 
 class DataLoader:
 
     data_path = "./data"
+    batch_size = 64
 
     mnist_trainset = datasets.MNIST(
         root=data_path, 
@@ -21,25 +24,40 @@ class DataLoader:
         transform=transform.ToTensor()
     )
     
+    
+    train_loader = DataLoader(
+            dataset= mnist_trainset,
+            batch_size= batch_size,
+            shuffle=True  
+    )
+
+    test_loader = DataLoader(
+            dataset= mnist_testset,
+            batch_size = batch_size,
+            shuffle=False
+    )
+
     #scales the input according to the given formula
     def normalization(self, data: np.ndarray):
         min, max = np.min(data), np.max(data)
         for i in range(len(data)):
             for j in range(len(data[i])):
                 data[i][j] = 2 * (data[i][j] - min) / (max - min) -1
+        
+        nn.CrossEntropyLoss
         return data
 
     
     def getSampleData(self):
         random = randint(0, len(self.mnist_trainset))
-        data = self.mnist_trainset.data[0].float().numpy()
+        data = self.mnist_trainset.data[random].float().numpy()
         data = self.normalization(data)
         return data.flatten()
 
-    def getNormalizedTrainData(self):
+    def normalizeData(self, image):
         normalize_data = []
-        for i in range (len(self.mnist_trainset.data)):
-            data = self.mnist_trainset.data[i].float().numpy()
+        for i in range (len(image)):
+            data = image[i].float().numpy()
             data = self.normalization(data)
             normalize_data.append(data)
         
--- Layer.py
+++ Layer.py
@@ -3,19 +3,18 @@ from Node import Node
 
 class Layer:
     #Initialize the layer with abritrary nodes
-    def __init__(self, numberOfNodes: int, inputData: np.ndarray,):
+    def __init__(self, numberOfNodes: int, numberOfInput):
         self.numberOfNodes = numberOfNodes
         self.nodes = [Node() for _ in range(numberOfNodes)]
         self.output = []
-        self.inputData = inputData
-        self.weights = np.random.uniform(low = -1, high = 1, size=(numberOfNodes, len(inputData)))
-        self.bias = np.random.uniform(low = -5, high= 5, size=(numberOfNodes))
+        self.weights = np.random.uniform(low = -1, high = 1, size=(numberOfNodes, numberOfInput))
+        self.bias = np.random.uniform(low = -2, high= 2, size=(numberOfNodes))
 
     #apply the linear transformation to each node
-    def linear(self):
+    def linear(self, inputData: np.ndarray):
         for i in range(self.numberOfNodes):
             node = self.nodes[i]
-            transformed = node.linear_transformation(self.inputData, self.weights[i], self.bias[i])
+            transformed = node.linear_transformation(inputData, self.weights[i], self.bias[i])
             self.output.append(transformed)    
 
     def applySigmoid(self):
